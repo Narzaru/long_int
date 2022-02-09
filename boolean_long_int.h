2ab750be30df95f8b26bda9ac27bae8a2cd1f764
Narzaru <yaroslahek@gmail.com>
2022-02-10 00:30:32 +0700

    bgreydon/implement all boolean operations

--- boolean_long_int.h
+++ boolean_long_int.h
@@ -5,16 +5,18 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-bool is_equal_long_int(int8_t *num1, int8_t *num2, size_t arrays_size);
+#include "types_long_int.h"
 
-bool is_not_equal_long_int(int8_t *num1, int8_t *num2, size_t arrays_size);
+bool is_equal_long_int(long_int_t num1, long_int_t num2);  
 
-bool is_less_long_int(int8_t *num1, int8_t *num2, size_t arrays_size);
+bool is_not_equal_long_int(long_int_t num1, long_int_t num2); 
 
-bool is_less_or_equal(int8_t *num1, int8_t *num2, size_t arrays_size);
+bool is_less_long_int(long_int_t num1, long_int_t num2);  
 
-bool is_greater(int8_t *num1, int8_t *num2, size_t arrays_size);
+bool is_less_or_equal_long_int(long_int_t num1, long_int_t num2); 
 
-bool is_greater_or_equal(int8_t *num1, int8_t *num2, size_t arrays_size);
+bool is_greater_long_int(long_int_t num1, long_int_t num2);  
+
+bool is_greater_or_equal_long_int(long_int_t num1, long_int_t num2);  
 
 #endif  // LONG_INT_BOOLEAN_H_
