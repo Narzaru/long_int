58593df65f0e1cf2b14c458246eedb4408c09e6a
Narzaru <yaroslahek@gmail.com>
2022-02-09 22:31:12 +0700

    narzaru/arithmetic_long_int.c:fix function name

--- arithmetic_long_int.c
+++ arithmetic_long_int.c
@@ -61,7 +61,7 @@ code_status_t sub_long_int(long_int_t num1, long_int_t num2, long_int_t *dst) {
     return ret_val;
 }
 
-code_status_t div_long_int(long_int_t num1, long_int_t num2, long_int_t *dst) {
+code_status_t mul_long_int(long_int_t num1, long_int_t num2, long_int_t *dst) {
     code_status_t ret_val;
     long_int_t tmp;
 
@@ -81,8 +81,4 @@ code_status_t div_long_int(long_int_t num1, long_int_t num2, long_int_t *dst) {
     }
 
     return ret_val;
-}
-
-code_status_t mul_long_int(long_int_t num1, long_int_t num2, long_int_t *dst) {
-
 }
